0f7455ebdd39a7b19e3b0d16922a4d43aa074f19
MartaRecio <marrecgil@alum.us.es>
2025-11-05 13:17:52 +0100

    Diseño web inicial

--- natursur/home/views.py
+++ natursur/home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+
+def home_view(request: HttpRequest) -> HttpResponse:
+    """
+    Renderiza la plantilla base de la página de inicio.
+    """
+    # Busca la plantilla en 'home/templates/home/index.html'
+    return render(request, 'home/index.html', {})
--- natursur/natursur/urls.py
+++ natursur/natursur/urls.py
@@ -15,13 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 
-from home import views
 
 
 urlpatterns = [
-    ##path('/', views.index),
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
 ]
