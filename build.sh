912cbd05ee65ce7dd1e8a6a6e2e3fa7e7d90aee5
MartaRecio <marrecgil@alum.us.es>
2025-12-03 13:43:55 +0100

    cambios build.sh

--- build.sh
+++ build.sh
@@ -2,12 +2,6 @@
 set -o errexit
 
 pip install -r requirements.txt
-
 cd natursur
-
 python manage.py collectstatic --no-input
-
-# Solo este comando para limpiar el error
-echo "TRUNCATE django_content_type CASCADE;" | python manage.py dbshell 2>/dev/null || true
-
 python manage.py migrate --noinput
